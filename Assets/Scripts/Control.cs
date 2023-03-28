2fea82047d441f153756e189c27d528976d58087
danigil <dgilkarov33@gmail.com>
2023-03-28 23:26:07 +0300

    trying to fix linting

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class Control : MonoBehaviour
 {
     Vector3 Vec;
+    float speed = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Vec = transform.localPosition;
-        Vec.y += Input.GetAxis("Vertical") * Time.deltaTime * 20;
-        Vec.x += Input.GetAxis("Horizontal") * Time.deltaTime * 20;
+        Vec.y += Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        Vec.x += Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         transform.localPosition = Vec;
     }
 }
